20969c7664ed370f58ed53e2daaf255d0c95d661
frankweb2 <frank.morales.ing.informatica@gmail.com>
2017-05-15 16:55:13 -0400

    primera aplicacion de Laravel 5.1

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,33 @@
+<?php
+
+return array(
+    'errors_title' => 'Por favor corrige los siguientes errores',
+    'login_title' => 'Inicio de sesión',
+    'login_button' => 'Inicia sesión',
+    'register_title' => 'Registro',
+    'register_button' => '¡Registrate!',
+    'remember' => '¡Recúerdame!',
+    'forgot_link' => "¿Olvidaste tu contraseña?",
+    );
+
+/*
+return [
+*/
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+/*
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+*/
+/*
+
+];
+*/
---
+++ resources/views/emails/password.blade.php
@@ -0,0 +1,3 @@
+<!-- resources/views/emails/password.blade.php -->
+
+<p>Click here to reset your password: {{ url('http://example.com/password/reset/'.$token) }}</p>
